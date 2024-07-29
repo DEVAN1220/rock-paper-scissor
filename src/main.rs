cf0805871430e63aca738868aefc6e26461bd561
DEVAN1220 <darshandevan50@gmail.com>
2024-07-29 20:09:07 +0530

    added base code

---
+++ src/main.rs
@@ -0,0 +1,109 @@
+use std::i32;
+use raylib::prelude::*;
+use rand::Rng;
+// random movement 
+// collsion 
+// select position
+// use images
+
+const WINDOW_WIDTH: i32 = 640;
+const WINDOW_HEIGHT: i32 = 480;
+const SIZE: f32 = 10.0;
+
+#[derive(Clone,Debug,PartialEq)]
+enum ObjectTypes {
+    Rock,
+    Paper,
+    Scissor
+}
+
+
+#[derive(Clone)]
+struct Object {
+    obj_type: ObjectTypes,
+    position: Vector2,
+}
+
+impl Object {
+
+    fn new(types: ObjectTypes, pos: Vector2) -> Object {
+        return Object { obj_type: types, position: pos};
+    }
+    fn beatable(&self) -> ObjectTypes {
+        // if &self.obj_type == ObjectTypes::Rock {
+        //     
+        // } else if &self.obj_type == ObjectTypes {
+        //     
+        // }
+        match self.obj_type {
+            ObjectTypes::Rock => return ObjectTypes::Scissor,
+            ObjectTypes::Scissor => return ObjectTypes::Paper,
+            ObjectTypes::Paper => return ObjectTypes::Rock
+        }
+    }
+    fn draw(&self,d: &mut RaylibDrawHandle) {
+        let color: Color;
+        match self.obj_type {
+            ObjectTypes::Rock => color = Color::BLACK,
+            ObjectTypes::Paper => color = Color::GRAY,
+            ObjectTypes::Scissor => color = Color::RED
+        }
+        d.draw_circle(self.position.x as i32, self.position.y as i32,SIZE, color);
+    }
+    fn update(&mut self, objects: &Vec<Object>) {
+        for object in objects {
+            if object.obj_type == self.beatable() {
+                let mut towards = Vector2 { x: (object.position.x - self.position.x), y: (object.position.y - self.position.y)};
+                towards.normalize();
+                let nx = self.position.x + towards.x;
+                let ny = self.position.y + towards.y;
+                if nx > SIZE && nx < WINDOW_WIDTH as f32 {
+                    self.position.x = nx;
+                }
+                if ny > SIZE && ny < WINDOW_HEIGHT as f32 {
+                    self.position.y = ny;
+                }
+            } else {
+                let mut towards = Vector2 { x: (object.position.x - self.position.x), y: (object.position.y - self.position.y)};
+                towards.normalize();
+                let nx = self.position.x - towards.x;
+                let ny = self.position.y - towards.y;
+                if nx > SIZE && nx < WINDOW_WIDTH as f32 {
+                    self.position.x = nx;
+                }
+                if ny > SIZE && ny < WINDOW_HEIGHT as f32 {
+                    self.position.y = ny;
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let  (mut rl, thread) = raylib::init()
+        .size(WINDOW_WIDTH, WINDOW_HEIGHT)
+        .title("rock paper scissor")
+        .build();
+
+    rl.set_target_fps(60);
+    let mut rng = rand::thread_rng();
+    let mut objects: Vec<Object>  = vec![];
+    objects.push(Object::new(ObjectTypes::Rock,Vector2 { x: rng.gen_range(SIZE..WINDOW_WIDTH as f32), y: rng.gen_range(SIZE..WINDOW_HEIGHT as f32)}));
+    objects.push(Object::new(ObjectTypes::Paper,Vector2 { x: rng.gen_range(SIZE..WINDOW_WIDTH as f32), y: rng.gen_range(SIZE..WINDOW_HEIGHT as f32)}));
+        objects.push(Object::new(ObjectTypes::Paper,Vector2 { x: rng.gen_range(SIZE..WINDOW_WIDTH as f32), y: rng.gen_range(SIZE..WINDOW_HEIGHT as f32)}));
+
+    let im_objects = objects.clone();
+    while !rl.window_should_close() {
+        // if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
+        //     x = rl.get_mouse_x();
+        //     y = rl.get_mouse_y();
+        //     println!("x: {}, y: {}",x, y);
+        // }
+        let mut d = rl.begin_drawing(&thread);
+        for object in &mut objects {
+            object.update(&im_objects);
+            object.draw(&mut d)
+        }
+        d.clear_background(Color::WHITE);
+    }
+}
